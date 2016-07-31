e89d7a2a00ecdf9ec4f9d093bf02c55cf367c75b
Hsiang <hasing0601@gmail.com>
2016-07-31 17:28:51 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
 import { SearchService } from'./app/search.service';
+import {HTTP_PROVIDERS} from '@angular/http';
 
 if (environment.production) {
   enableProdMode();
 }
 
 ////[Injection service];
-bootstrap(AppComponent, [SearchService]);
+bootstrap(AppComponent, [SearchService, HTTP_PROVIDERS]);
 
